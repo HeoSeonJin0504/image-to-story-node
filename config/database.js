dea15a70d10e51c7b680a8951a7f4ab629332b75
HeoSeonJin0504 <sonjin54@naver.com>
2026-02-21 22:12:54 +0900

    호스팅용 postgre sql 추가

--- config/database.js
+++ config/database.js
@@ -6,14 +6,34 @@ if (!process.env.DATABASE_URL) {
   throw new Error('DATABASE_URL 환경변수가 설정되지 않았습니다.');
 }
 
+// MySQL
+// const sequelize = new Sequelize(process.env.DATABASE_URL, {
+//   dialect: 'mysql',
+//   logging: false,
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     acquire: 30000,
+//     idle: 10000
+//   }
+// });
+
+// Supabase
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'mysql',
+  dialect: 'postgres',
+  dialectModule: require('pg'),
   logging: false,
   pool: {
     max: 5,
     min: 0,
     acquire: 30000,
     idle: 10000
+  },
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    }
   }
 });
 
